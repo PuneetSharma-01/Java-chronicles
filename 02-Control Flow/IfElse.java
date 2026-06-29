b34503bb24c77feb9217a081bb591a21e9af8382
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-06-30 03:31:09 +0530

    Started Another Topic Control Flow

---
+++ 02-Control Flow/IfElse.java
@@ -0,0 +1,17 @@
+public class IfElse {
+
+    static int i=20;
+    public static void main(String[]args)
+    {
+      if(i<10)
+      {
+        System.out.println("The Number is Less then 10");
+      }
+      else
+      {
+        System.out.println("The Number is greater than 10");
+
+      }
+    }
+    
+}
---
+++ 02-Control Flow/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+    static int i=10;
+    public static void main(String[]args)
+    {
+        if(i<10)
+        {
+        System.out.println("TRUE");
+        }
+    }
+
+    
+}
