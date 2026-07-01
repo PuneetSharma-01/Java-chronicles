03a23c28e7c001a098856197cac86fc7e2168bf5
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-02 02:54:29 +0530

    Added Do while loop and break statement

---
+++ 03-Control Flow/Break.java
@@ -0,0 +1,15 @@
+public class Break {
+    static int i=1;
+    public static void main(String[]args)
+    {
+        switch(i)
+        {
+            case 1:
+            System.out.println(i+2);
+            break;
+            case 2:
+            System.out.println(i+5);
+        }
+    }
+    
+}
---
+++ 03-Control Flow/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop
+{
+    static int i=0;
+    public static void main(String[]args)
+    {
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<5);
+    }
+}
