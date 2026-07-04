dc09b63ad34d4b1c9cfca6158ae898bd9b612ef4
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-05 00:42:33 +0530

    Added Assignment Operator Program

---
+++ 02-Operators Assignments/AssignmentOperators.java
@@ -0,0 +1,9 @@
+public class AssignmentOperators
+{
+    static int num=100;
+    public static void main(String[]args)
+    {
+        System.out.println("Value of num is " + num);
+        
+    }
+}
