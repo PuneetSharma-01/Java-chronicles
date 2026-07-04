d12bca924f068ee92a631ab98d04cc575a86d0b6
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-05 00:50:54 +0530

    Added Decrement Operator program

---
+++ 02-Operators Assignments/DecrementOperator.java
@@ -0,0 +1,10 @@
+public class DecrementOperator {
+    static int num=100;
+    public static void main(String[]args)
+    {
+        System.out.println("THe value of num is " + num);
+        num--;
+        System.out.println("The value of num after decrement is " + num);
+    }
+    
+}
