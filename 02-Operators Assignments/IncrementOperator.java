a61264d2f3c11bec2b84ad64088b435485bbdd90
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-05 00:46:43 +0530

    Added Increment Operator Program

---
+++ 02-Operators Assignments/IncrementOperator.java
@@ -0,0 +1,10 @@
+public class IncrementOperator {
+    static int num=100;
+    public static void main(String[]args)
+    {
+        System.out.println("Value of num is " + num);
+        num++;
+        System.out.println("Value of num after increment is " +num);
+    }
+    
+}
