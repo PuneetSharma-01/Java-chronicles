439d87757b1e0864085e67f665ddb4d986a45a7f
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-06 23:26:13 +0530

    Added Logical Operator Program

---
+++ 02-Operators Assignments/LogicalOperator.java
@@ -0,0 +1,12 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b = " + (a && b)); // AND
+        System.out.println("a || b = " + (a || b)); // OR
+        System.out.println("!a = " + (!a));         // NOT
+        System.out.println("!b = " + (!b));         // NOT
+    }
+}
