2d3e569d425b999ec182c85bb021c1ef92271aab
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-07 23:31:58 +0530

    Added Shift Operator

---
+++ 02-Operators Assignments/ShiftOperator.java
@@ -0,0 +1,11 @@
+public class ShiftOperator {
+    public static void main(String[] args) {
+
+        int num = 8;
+
+        System.out.println("Original Number: " + num);
+        System.out.println("Left Shift (num << 1): " + (num << 1));
+        System.out.println("Right Shift (num >> 1): " + (num >> 1));
+
+    }
+}
