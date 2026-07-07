4ecb3fcdf597947e53c02fe47a079777982bef43
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-07 23:36:07 +0530

    Added Unary Operator Program

---
+++ 02-Operators Assignments/UnaryOperator.java
@@ -0,0 +1,17 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        System.out.println("Original Number: " + num);
+
+        System.out.println("Increment: " + (++num));
+
+        System.out.println("Decrement: " + (--num));
+
+        System.out.println("Unary Plus: " + (+num));
+
+        System.out.println("Unary Minus: " + (-num));
+
+    }
+}
