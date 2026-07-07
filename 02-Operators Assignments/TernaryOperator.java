196eaa206d7836af7179ba3d42a09b56613bcddf
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-07 23:38:24 +0530

    Added Ternary Operator Program

---
+++ 02-Operators Assignments/TernaryOperator.java
@@ -0,0 +1,12 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 10;
+
+        int max = (num1 > num2) ? num1 : num2;
+
+        System.out.println("Greater Number = " + max);
+
+    }
+}
