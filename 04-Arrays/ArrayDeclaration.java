aa5ec23abba4ef0a0318e016417018d2a482dc8a
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-09 23:59:12 +0530

    Added Array Declaration and Initilization Program

--- 04-Arrays/ArrayDeclaration.java
+++
@@ -1,8 +0,0 @@
-public class ArrayDeclaration {
-    public static void main(String[]args)
-    {
-    int[] arr= new int[5];
-    System.out.println("Array declaration successfull");
-    }
-    
-}
---
+++ 04-Arrays/ArrayDeclarationAndInitialization.java
@@ -0,0 +1,14 @@
+public class ArrayDeclarationAndInitialization {
+    public static void main(String[]args)
+    {
+    int[] arr= new int [5]; // Declaration of an array
+    arr[0]=10;
+    arr[1]=20;
+    arr[2]=30;
+    arr[3]=40;
+    arr[4]=50;
+    System.out.println("Array initialization successful");
+
+    }
+    
+}
