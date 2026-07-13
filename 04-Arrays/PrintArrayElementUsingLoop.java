0d648bca82b161e6cb1e61b61a55a06576c05227
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-14 00:16:30 +0530

    Added Print Array Element Using Loop Program

---
+++ 04-Arrays/PrintArrayElementUsingLoop.java
@@ -0,0 +1,17 @@
+public class PrintArrayElementUsingLoop {
+    public static void main(String[]args)
+    {
+        int[]arr= new int[3];//Declaration of an array;
+        arr[0]=100;
+        arr[1]=200;
+        arr[2]=300;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
+
+    
+
