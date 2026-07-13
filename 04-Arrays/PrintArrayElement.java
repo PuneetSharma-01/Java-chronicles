c8165f20d3f261f6df01741e2881e3b363639b3e
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-13 23:56:26 +0530

    Added Print Array Element Program

---
+++ 04-Arrays/PrintArrayElement.java
@@ -0,0 +1,14 @@
+public class PrintArrayElement {
+    public static void main(String[]args)
+    {
+        int[]arr= new int[3];//Declaration of an array;
+        arr[0]=100;
+        arr[1]=200;
+        arr[2]=300;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
