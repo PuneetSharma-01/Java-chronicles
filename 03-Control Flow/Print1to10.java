85c78401682b12f9173ce2921d5d5723cd9aadaf
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-15 03:01:47 +0530

    Added Print Number Using Loop Program

---
+++ 03-Control Flow/Print1to10.java
@@ -0,0 +1,12 @@
+public class Print1to10 {
+    static int var1=10;
+    public static void main(String[] args) {
+        {
+            for(int i=1;i<=var1;i++)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
