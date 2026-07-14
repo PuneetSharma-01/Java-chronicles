272393dd3339fc6b4169ecd6b13a85af29803ceb
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-15 03:03:00 +0530

    added Largest Between  Two Program

---
+++ 03-Control Flow/LargestofTwo.java
@@ -0,0 +1,16 @@
+public class LargestofTwo {
+    static int num1=12,num2=56;
+    
+    public static void main(String[] args) {
+        if(num1>num2)
+        {
+            System.out.println(num1 + " is Largest ");
+        }
+        else
+        {
+            System.out.println(num2 + " is Largest ");
+        }
+        
+    }
+    
+}
