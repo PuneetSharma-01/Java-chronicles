9b57ba6d3bd9dc6e2efe5647f167fb6c44761d34
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-15 03:00:57 +0530

    Added Even or Odd Number Program

---
+++ 03-Control Flow/EvenorOdd.java
@@ -0,0 +1,16 @@
+public class EvenorOdd {
+    static int num=45;
+    public static void main(String[] args) {
+        {
+            if(num%2==0)
+            {
+                System.out.println("Even " +num);
+            }
+            else
+            {
+                System.out.println("Odd " +num);
+            }
+        }
+    }
+    
+}
