579fd7168335566e799b52e4634c21f3e0d0f26d
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-17 23:31:08 +0530

    Started OOPS Practice Program

---
+++ 05-OOPS/FirstObject.java
@@ -0,0 +1,16 @@
+public class FirstObject {
+   public  String name;
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args) {
+        {
+            FirstObject Obj= new FirstObject();
+            Obj.name="PUNEET SHARMA";
+            System.out.println(Obj.getName());
+            
+
+        }
+    }
+    
+}
