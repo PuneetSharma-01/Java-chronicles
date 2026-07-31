063208c80fbaae6a3c37637d44d56903a19e8c88
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-31 11:43:38 +0530

    Update Parameterized Constructor Program

--- 05-OOPS/02-CONSTRUCTOR/ParameterizedConstructor.java
+++ 05-OOPS/02-CONSTRUCTOR/ParameterizedConstructor.java
@@ -1,16 +1,17 @@
 public class ParameterizedConstructor {
-    private String name;
-    private int age;
+     String name;
+     int age;
     ParameterizedConstructor(String name, int age)//Constructor
     {
         this.name=name;
         this.age=age;
-        System.out.println(name+"..."+age);
     }
     public static void main (String[]args)
     {
         ParameterizedConstructor d1= new ParameterizedConstructor("Puneet Sharma", 19);
         ParameterizedConstructor d2= new ParameterizedConstructor("InderJot Riar", 21);
+        System.out.println(d1.name+".."+d1.age);
+        System.out.println(d2.name+".."+d2.age);
     }
     
 }
