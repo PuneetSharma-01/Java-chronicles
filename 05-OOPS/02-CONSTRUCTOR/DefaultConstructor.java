6a077e23393b8d40e170bc24f8455f7b9d6a9e99
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-31 10:48:26 +0530

    Added better version of Default Constructor Program

--- 05-OOPS/02-CONSTRUCTOR/DefaultConstructor.java
+++ 05-OOPS/02-CONSTRUCTOR/DefaultConstructor.java
@@ -1,13 +1,15 @@
 public class DefaultConstructor {
+    int i=10;
     
     DefaultConstructor()//Constructor
     {
-        System.out.println("Defaulr Constructor Initiatted");
-
+     //Default Constructor
+     System.out.println("IS CREATED");
     }
     public static void main (String[]args)
     {
         DefaultConstructor d1= new DefaultConstructor();
+        System.out.println(d1.i);
     }
 }
 
