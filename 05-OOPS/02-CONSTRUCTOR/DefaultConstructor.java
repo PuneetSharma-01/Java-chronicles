f964627f87600f22fb08e57599d34a4475f3ce8c
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-07-31 09:30:19 +0530

    Added Default Constructor Program

---
+++ 05-OOPS/02-CONSTRUCTOR/DefaultConstructor.java
@@ -0,0 +1,13 @@
+public class DefaultConstructor {
+    
+    DefaultConstructor()//Constructor
+    {
+        System.out.println("Defaulr Constructor Initiatted");
+
+    }
+    public static void main (String[]args)
+    {
+        DefaultConstructor d1= new DefaultConstructor();
+    }
+}
+
