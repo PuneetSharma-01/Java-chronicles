d9a207b20723e49dd36ea409890f86c4377d2e87
PuneetSharma-1 <PuneetSharmax01@gmail.com>
2026-08-20 04:10:03 +0530

    Added Join Keyword Program

---
+++ 07-MultiThreading/01-ThreadBasics/Threadjoin.java
@@ -0,0 +1,28 @@
+class MyThread extends Thread {
+    @Override
+    public void run()
+    {
+        for(int i=0;i<2;i++)
+        {
+            System.out.println("Singinggggggggg");
+        }
+    }
+}
+public class Threadjoin {
+    public static void main(String[]args)
+    {
+     MyThread t1=new MyThread();
+     MyThread t2=new MyThread();
+     t1.start();
+     try{
+     t1.join();
+     }
+     catch(InterruptedException ie)
+     {
+        System.out.println("Thread was Interrupted");
+     }
+     t2.start();
+     
+    }
+}
+
